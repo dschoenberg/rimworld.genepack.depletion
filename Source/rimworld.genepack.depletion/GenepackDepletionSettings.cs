8e62fcba5c14679b15cf6628e8c08d185e80f6e2
dschoenberg <dschoenberg@gmail.com>
2022-11-25 18:01:07 -0600

    Settings & readme updates

--- Source/rimworld.genepack.depletion/GenepackDepletionSettings.cs
+++ Source/rimworld.genepack.depletion/GenepackDepletionSettings.cs
@@ -4,15 +4,15 @@ namespace rimworld.genepack.depletion
 {
     class GenepackDepletionSettings : ModSettings
     {
-        public int minDepletionPerComplexity = 20;
-        public int maxDepletionPerComplexity = 33;
+        public int minDepletionPerComplexity = 15;
+        public int maxDepletionPerComplexity = 25;
         public bool applyEvenly = false;
 
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref minDepletionPerComplexity, "minDepletionPerComplexity", 20);
-            Scribe_Values.Look(ref maxDepletionPerComplexity, "maxDepletionPerComplexity", 33);
+            Scribe_Values.Look(ref minDepletionPerComplexity, "minDepletionPerComplexity", 15);
+            Scribe_Values.Look(ref maxDepletionPerComplexity, "maxDepletionPerComplexity", 25);
             Scribe_Values.Look(ref applyEvenly, "applyEvenly", false);
         }
     }
